207f7d661e59de0ab21f5ff982e3023e43d2f04e
Bruno Rodrigues <brunoresenderodrigues@gmail.com>
2025-03-19 14:39:00 -0300

    Define a job

--- examples/RANO/data_preparator/project/rano_dagster/definitions.py
+++ examples/RANO/data_preparator/project/rano_dagster/definitions.py
@@ -5,8 +5,11 @@ from dagster_docker import PipesDockerClient
 
 rano_assets = dg.load_assets_from_modules([assets])
 
+rano_pipeline_job = dg.define_asset_job(name="rano_pipeline_job")
+
 defs = dg.Definitions(
     assets=rano_assets,
     sensors=[file_sensor],
     resources={"docker_pipes_client": PipesDockerClient()},
+    jobs=[rano_pipeline_job],
 )
