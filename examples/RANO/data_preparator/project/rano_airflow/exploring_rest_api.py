88bc885150e8bda9d3d940ebf9e77268bd95218e
Bruno Rodrigues <brunoresenderodrigues@gmail.com>
2025-04-03 16:18:32 -0300

    Make the Rest API exploration just a bit faster

--- examples/RANO/data_preparator/project/rano_airflow/exploring_rest_api.py
+++ examples/RANO/data_preparator/project/rano_airflow/exploring_rest_api.py
@@ -69,11 +69,11 @@ def sort_dag_or_task_list(dag_or_task_list):
 
 
 def _get_tag_to_dags_dict(
-    dags_raw_result: DAGCollection,
+    dags_raw_result: DAGCollection, include_untagged: bool = False
 ) -> dict[str, list[dict[str, str]]]:
     tag_to_dag_dict = defaultdict(lambda: list())
     for dag in dags_raw_result["dags"]:
-        if not dag["tags"]:
+        if not dag["tags"] and include_untagged:
             tag_to_dag_dict["Untagged"].append(dag)
             continue
 
@@ -83,17 +83,15 @@ def _get_tag_to_dags_dict(
     return tag_to_dag_dict
 
 
-with client.ApiClient(configuration) as api_client:
+with client.ApiClient(configuration, pool_threads=50) as api_client:
 
     dag_api_instance = dag_api.DAGApi(api_client)
     dag_run_api_instance = dag_run_api.DAGRunApi(api_client)
-    task_instance_api_instance = task_instance_api.TaskInstanceApi(api_client)
 
     dags = wrapped_api_call(
         dag_api_instance.get_dags,
-        # only_active=True,
-        order_by="dag_id",
-        # fields=["dag_id", "dag_display_name", "tags"],
+        only_active=True,
+        fields=["dag_id", "tags"],
     )
 
     tag_to_dags_dict = _get_tag_to_dags_dict(dags)
@@ -105,17 +103,18 @@ with client.ApiClient(configuration) as api_client:
         sorted_key: tag_to_dags_dict[sorted_key] for sorted_key in sorted_tags
     }
 
-    tag_to_running_info = {tag: [] for tag in tag_to_dags_dict}
+    tag_to_dag_to_threads = {}
+    # Initialize requests
     for tag, dag_list in tag_to_dags_dict.items():
-
+        tag_to_dag_to_threads[tag] = {}
         for dag in dag_list:
             dag_id = dag["dag_id"]
-
-            # TODO looking at dag_runs only, I don't get unstarted DAGs
-            # Should probably query both, filter out started, mark the
-            # unstarted DAGS accordingly
-            dag_runs = wrapped_api_call(
-                dag_run_api_instance.get_dag_runs,
+            dag_details_thread = dag_api_instance.get_dag_details(
+                dag_id=dag_id,
+                async_req=True,
+                fields=["dag_id", "dag_display_name", "doc_md"],
+            )
+            dag_runs_thread = dag_run_api_instance.get_dag_runs(
                 dag_id=dag_id,
                 order_by="-execution_date",  # Most recent first
                 fields=[
@@ -126,40 +125,67 @@ with client.ApiClient(configuration) as api_client:
                     "end_date",
                 ],  # TODO validate start/end times of dag run
                 limit=1,
+                async_req=True,
             )
-            dag_runs = dag_runs["dag_runs"]
-            dag_task_definitions = wrapped_api_call(
-                dag_api_instance.get_tasks, dag_id=dag_id
+            dag_task_definitions_thread = dag_api_instance.get_tasks(
+                dag_id=dag_id, async_req=True
             )
-            dag_task_definitions = dag_task_definitions["tasks"]
 
-            if dag_runs:
-                task_id_to_doc_md = {
-                    task["task_id"]: task["doc_md"] for task in dag_task_definitions
-                }
-                most_recent_run = dag_runs[0]
-                dag_run_id = most_recent_run["dag_run_id"]
-                dag_run_tasks = wrapped_api_call(
-                    task_instance_api_instance.get_task_instances,
-                    dag_id=dag_id,
-                    dag_run_id=dag_run_id,
+            tag_to_dag_to_threads[tag][dag["dag_id"]] = {
+                "details": dag_details_thread,
+                "runs": dag_runs_thread,
+                "tasks": dag_task_definitions_thread,
+            }
+
+
+dags_with_runs = []
+tag_to_running_info = {tag: [] for tag in tag_to_dags_dict}
+for tag, dag_dict in tag_to_dag_to_threads.items():
+    for dag_id, dag_info_dict in dag_dict.items():
+        base_dag_dict = dag_info_dict["details"].get().to_dict()
+        dag_runs = dag_info_dict["runs"].get().to_dict()["dag_runs"]
+        task_definitions = dag_info_dict["tasks"].get().to_dict()["tasks"]
+        base_dag_dict["tasks"] = task_definitions
+
+        if dag_runs:
+            base_dag_dict["run"] = dag_runs[0]
+            dags_with_runs.append(base_dag_dict)
+
+        else:
+            for dag_task in base_dag_dict["tasks"]:
+                dag_task["state"] = State.PENDING
+
+            base_dag_dict["state"] = State.PENDING
+
+        tag_to_running_info[tag].append(base_dag_dict)
+
+if dags_with_runs:
+    with client.ApiClient(configuration, pool_threads=50) as api_client:
+        task_instance_api_instance = task_instance_api.TaskInstanceApi(api_client)
+        for dag in dags_with_runs:
+            run_info = dag.pop("run")
+            dag["state"] = run_info["state"]
+            dag["task_instances_thread"] = (
+                task_instance_api_instance.get_task_instances(
+                    dag_id=dag["dag_id"],
+                    dag_run_id=run_info["dag_run_id"],
+                    async_req=True,
                 )
-                dag_run_tasks = dag_run_tasks["task_instances"]
-                for dag_run_task in dag_run_tasks:
-                    dag_run_task["doc_md"] = task_id_to_doc_md[dag_run_task["task_id"]]
-                dag_run_tasks = sort_dag_or_task_list(dag_run_tasks)
-                most_recent_run["task_instances"] = dag_run_tasks
-                tag_to_running_info[tag].append(most_recent_run)
-            else:
-                for dag_task in dag_task_definitions:
-                    dag_task["state"] = State.PENDING
+            )
 
-                dag["state"] = State.PENDING
-                dag["task_instances"] = dag_task_definitions
-                tag_to_running_info[tag].append(dag)
+    for dag in dags_with_runs:
+        task_instances = dag.pop("task_instances_thread").get().to_dict()
+        task_instances = task_instances["task_instances"]
+        task_definitions = dag.pop("tasks")
+        task_id_to_doc_md = {
+            task["task_id"]: task["doc_md"] for task in task_definitions
+        }
+        for task_instance in task_instances:
+            task_instance["doc_md"] = task_id_to_doc_md[task_instance["task_id"]]
+        task_instances = sort_dag_or_task_list(task_instances)
+        dag["tasks"] = task_instances
 
-            tag_to_running_info[tag] = sort_dag_or_task_list(tag_to_running_info[tag])
-
-# print(tag_to_running_info)
+for tag, dag_list in tag_to_running_info.items():
+    tag_to_running_info[tag] = sort_dag_or_task_list(dag_list)
 end = time.perf_counter()
 print(f"Time elapsed: {end-start}s")
