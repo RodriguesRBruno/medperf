0c3012ae8348f02bb3e28be0caa134f4ab817a93
Bruno Rodrigues <brunoresenderodrigues@gmail.com>
2025-04-09 11:47:32 -0300

    Remove REPORT_LOCK at the start of the pipeline if it is present

--- examples/RANO/data_preparator/project/stages/env_vars.py
+++ examples/RANO/data_preparator/project/stages/env_vars.py
@@ -6,4 +6,4 @@ DATA_DIR = os.path.join(WORKSPACE_DIR, "data")
 DATA_SUBDIR = os.path.join(*DATA_DIR.split(os.sep)[-2:])
 INPUT_DIR = os.path.join(WORKSPACE_DIR, "input_data")
 REPORT_PATH = os.getenv("REPORT_FILE") or os.path.join(WORKSPACE_DIR, "report.yaml")
-REPORT_LOCK = os.path.splitext(REPORT_PATH)[0] + ".lock"
+REPORT_LOCK = os.path.dirname(REPORT_PATH) + ".lock"
